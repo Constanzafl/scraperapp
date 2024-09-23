a66fe5808a9e105af78245a36d473976234e3c5e
constanza florio <constanzaflorio@librodepases.com>
2024-09-23 02:09:52 -0300

    o

--- main.py
+++ main.py
@@ -4,5 +4,31 @@ st.title("AI web scraper con llms")
 url=st.text_input("Enter the URL:")
 
 if st.button("Srape Site"):
-    st.write("Scraping site now")
+    if url:
+        st.write("Scraping site now")
 
+# Scrape the website
+        dom_content = scrape_website(url)
+        body_content = extract_body_content(dom_content)
+        cleaned_content = clean_body_content(body_content)
+
+        # Store the DOM content in Streamlit session state
+        st.session_state.dom_content = cleaned_content
+
+        # Display the DOM content in an expandable text box
+        with st.expander("View DOM Content"):
+            st.text_area("DOM Content", cleaned_content, height=300)
+
+
+# Step 2: Ask Questions About the DOM Content
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content...")
+
+            # Parse the content with Ollama
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            parsed_result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(parsed_result)    
